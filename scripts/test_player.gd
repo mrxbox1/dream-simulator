a22d6c8d2bc8116b9732a5eead5bc51d8fd5ccf9
ukraine584 <48248684+mrxbox1@users.noreply.github.com>
2025-11-21 22:47:49 +0100

    Finally added mouse controls
    I got sick of using arrow keys to rotate the camera, so I implemented mouse controls. The mouse is now hidden, and moving it will change the camera's rotation.
    
    Thanks to rbarongr on GitHub for creating a first-person controller for Godot 4 which I pinched some code from. You can see rbarongr's repository for the first-person controller here: https://github.com/rbarongr/GodotFirstPersonController/

--- scripts/test_player.gd
+++ scripts/test_player.gd
@@ -3,10 +3,10 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY = 0.01
 
 func _ready() -> void:
-	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	pass
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,6 +21,10 @@ func _physics_process(delta: float) -> void:
 		rotate_y(5 * delta)
 	if Input.is_action_pressed("ui_right"):
 		rotate_y(-5 * delta)
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED: Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else: Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -34,3 +38,14 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+# Thank you to rbarongr on GitHub for creating a First Person Controller which I pinched some code from, as you can see below.
+# You can see their repository here: https://github.com/rbarongr/GodotFirstPersonController/tree/main
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		#print(event.relative)
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		$Camera3D.rotation.x = clamp(
+			$Camera3D.rotation.x - event.relative.y * MOUSE_SENSITIVITY, 
+			-1.5, 1.5
+		)
