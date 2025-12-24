c958168354a8616c0c78497d4f46e16f4a2b7ea0
mrxbox1 <gabriel.holub@outlook.com>
2025-12-24 12:22:12 +0100

    added "destroy target" option in test_button script

--- scripts/test_button.gd
+++ scripts/test_button.gd
@@ -1,10 +1,11 @@
 extends Area3D
 
 @export var target_node: Node
-@export_enum("Enable target", "Disable target", "Toggle target") var signal_sent: int
+@export_enum("Enable target", "Disable target", "Toggle target", "Destroy target (and self)") var signal_sent: int
 
 func trigger():
 	match signal_sent:
 		0: target_node.enable()
 		1: target_node.disable()
 		2: target_node.toggle()
+		3: target_node.queue_free(); self.queue_free()
